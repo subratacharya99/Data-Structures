bd4b9261b4705f8d282246578e216c73957fdaf0
subratacharya99 <subratacharya99@gmail.com>
2022-05-21 17:05:13 -0400

    added inplace quicksort and fib

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,6 @@
+"""
+O(nlogn)
+"""
 def mergesort(nums):
     if len(nums) > 1:
         left = nums[:len(nums)//2]
@@ -26,9 +29,77 @@ def mergesort(nums):
     return nums
 
 
+"""
+Average time complexity: O(nlogn)
+Worst case time complexity: O(n^2)
+Requires extra space
+"""
+def quicksort(nums):
+    if len(nums) <= 1:
+        return nums
+    
+    pivot = len(nums)//2
+    left = []
+    right = []
+
+    for i in range(len(nums)):
+        if nums[i] < nums[pivot]:
+            left.append(nums[i])
+        if nums[i] > nums[pivot]:
+            right.append(nums[i])
+
+    return quicksort(left) + [pivot] + quicksort(right)
+
+
+"""
+Average time complexity: O(nlogn)
+Worst case time complexity: O(n^2)
+In place sorting
+"""
+def quicksort2(arr, left, right):
+    if left < right:
+        pivot = partition(arr, left, right)
+        quicksort2(arr, left, pivot-1)
+        quicksort2(arr, pivot+1, right)
+
+def partition(arr, left, right):
+    i = left
+    j = right-1
+    pivot = arr[right]
+
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i+=1
+        while j > left and arr[j] >= pivot:
+            j-=1
+        
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+        
+    if arr[i] > pivot:
+        arr[i], arr[right] = arr[right], arr[i]
+    return i
+
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+memo = {0:0, 1:1}
+def fib(num):
+    if num in memo:
+        return memo[num]
+    
+    ans = fib(num-1) + fib(num-2)
+    memo[num] = ans
+    return ans
+
 
 
 
 if __name__ == '__main__':
-    arr = [1,37,3,12,45,6,8,13,55]
-    print(mergesort(arr))
+    arr = [1,5,5,3,4,5,5,2,2,3,4,2,2,1,1,1,2,2,3,3,4,5]
+    # quicksort2(arr, 0, len(arr)-1)
+    # print(arr)
+
+    print(fib(29))
